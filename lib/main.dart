91baefc220d107eb9752f18599208d61a0913a60
Abdulrasol <abdulrsol97@gmail.com>
2024-08-27 23:58:53 +0300

    add location pub

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
-        ),
+            child: FutureBuilder(
+          future:
+              Future.delayed(const Duration(seconds: 3)).then((onValue) => 't'),
+          builder: (BuildContext context, dynamic snapshot) {
+            if (snapshot.hasData) {
+              return Center(child: Text('snapshot'));
+            }
+
+            return const CircularProgressIndicator();
+          },
+        )),
       ),
     );
   }
