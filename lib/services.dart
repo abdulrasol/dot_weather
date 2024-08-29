1e728fc16bb3c09e4d218b456b5b0d8eeb5848c8
AbdulRasol S. Al-Hilo <abdulrsol97@gmail.com>
2024-08-29 12:37:37 +0300

    comment

--- lib/services.dart
+++ lib/services.dart
@@ -24,12 +24,15 @@ Future<Map<String, dynamic>> getLocation() async {
 
 Future getWeather({required latitude, required longitude}) async {
   const String scrent = 'f5b1de87731f4d70b2e124743242808';
-  print(scrent);
+  //print(scrent);
   final dio = Dio();
   Response data = await dio.get(
-      "http://api.weatherapi.com/v1/forecast.json?key$scrent=&q=$latitude,$longitude&days=1&aqi=no&alerts=no");
-  Map weather = jsonDecode(data.data.toString());
-  Map weatherData = {
+      "http://api.weatherapi.com/v1/forecast.json?key=$scrent&q=$latitude,$longitude&days=1&aqi=no&alerts=no");
+  print(data.realUri.toString());
+  print(data.data.runtimeType);
+  //Map weather = jsonDecode(data.data);
+  //print(weather.entries);
+  /*Map weatherData = {
     'city': weather['location']['name'],
     'current_temp': weather['current']['temp_c'],
     'current_condition': weather['current']['condition']['text'],
@@ -41,7 +44,7 @@ Future getWeather({required latitude, required longitude}) async {
     'day_icon': weather['forecast']['forecastday'][0]['day']['condition']
         ['icon'],
     'hours': weather['forecast']['forecastday'][0]['huor']['condition']['icon'],
-  };
-  print(weather.runtimeType);
+  };*/
+  // print(weather.runtimeType);
   return data;
 }
